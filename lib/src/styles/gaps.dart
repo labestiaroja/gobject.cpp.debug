5a7e5c7c4dfa43d81e43c200c09f76a7428a3a52
labestiaroja <labestiaroja@proton.me>
2024-10-05 00:27:51 +0300

    feat: add gaps style

---
+++ lib/src/styles/gaps.dart
@@ -0,0 +1,9 @@
+import 'package:gap/gap.dart';
+
+const Gap gap4 = Gap(4.0);
+const Gap gap8 = Gap(8.0);
+const Gap gap16 = Gap(16.0);
+const Gap gap32 = Gap(32.0);
+
+const Gap gap10 = Gap(10.0);
+const Gap gap20 = Gap(20.0);
