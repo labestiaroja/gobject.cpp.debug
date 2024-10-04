ee95581676333575aea2e4d9e543a3181a369cbb
labestiaroja <labestiaroja@proton.me>
2024-10-05 01:53:25 +0300

    refactor: start gap styles with k

--- lib/src/styles/gaps.dart
+++ lib/src/styles/gaps.dart
@@ -1,9 +1,9 @@
 import 'package:gap/gap.dart';
 
-const Gap gap4 = Gap(4.0);
-const Gap gap8 = Gap(8.0);
-const Gap gap16 = Gap(16.0);
-const Gap gap32 = Gap(32.0);
+const Gap kGap4 = Gap(4.0);
+const Gap kGap8 = Gap(8.0);
+const Gap kGap16 = Gap(16.0);
+const Gap kGap32 = Gap(32.0);
 
-const Gap gap10 = Gap(10.0);
-const Gap gap20 = Gap(20.0);
+const Gap kGap10 = Gap(10.0);
+const Gap kGap20 = Gap(20.0);
