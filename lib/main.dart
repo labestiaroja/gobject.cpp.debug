d2149e2d933f4910a5efe656843ef95fd17a6c05
labestiaroja <labestiaroja@proton.me>
2024-10-05 01:52:18 +0300

    feat: register debug service implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/widgets.dart';
 import 'package:gobjdebug/src/app.dart';
+import 'package:gobjdebug/src/pigeon/debug_service.g.dart';
+import 'package:gobjdebug/src/service/debug_service_impl.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  DebugServiceFlutterApi.setUp(DebugServiceFlutterApiImpl());
+
   runApp(const App());
 }
